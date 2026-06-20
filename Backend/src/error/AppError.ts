3a0575c783c701da68c84633a6130bcd5ed1f66f
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-06-20 12:08:30 +0530

    setup backend

--- Backend/src/error/AppError.ts
+++
@@ -1,8 +0,0 @@
-export class AppError extends Error {
-  public readonly statusCode: number;
-
-  constructor(statusCode: number, message: string) {
-    super(message);
-    this.statusCode = statusCode;
-  }
-}
--- Backend/src/utils/asyncHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction } from "express";
-
-export function asyncHandler(
-  func: (req: Request, res: Response, next: NextFunction) => Promise<void>,
-) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    func(req, res, next).catch(next);
-  };
-}
